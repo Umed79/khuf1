4133f9087cf752e193619d43062ecea484e30c83
Umed79 <65124332+Umed79@users.noreply.github.com>
2020-05-28 15:56:51 +0500

    Add files via upload

---
+++ khuf valley.js
@@ -0,0 +1,5 @@
+const hamburger =document.querySelector(".hamburger");
+const navmobi2 =document.getElementById("nav-mobi2");
+hamburger.addEventListener("click",function(){
+    navmobi2.classList.toggle("nav-mobi1")
+})
